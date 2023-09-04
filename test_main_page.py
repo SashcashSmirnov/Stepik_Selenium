3d7c37482af1438c4069999f147c0b517cc7011b
Sashcash <k079kk@yandex.ru>
2023-09-04 15:55:32 +0500

    redacted test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -13,6 +13,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from pages.main_page import MainPage
 
 
+def test_guest_can_see_login_page(browser):
+
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(browser):
 
     link = "http://selenium1py.pythonanywhere.com/"
